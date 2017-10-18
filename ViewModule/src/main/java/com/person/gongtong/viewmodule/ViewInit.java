207786285beb9157b5faf0a76f1a3cd0b7ff1d71
gongtong <gongtong@2dfire.com>
2017-10-19 00:03:52 +0800

    init

---
+++ ViewModule/src/main/java/com/person/gongtong/viewmodule/ViewInit.java
@@ -0,0 +1,11 @@
+package com.person.gongtong.viewmodule;
+
+/***********************************************
+ * <P> desc:
+ * <P> Author: gongtong
+ * <P> Date: 2017/10/18 18:37
+ * <P> Copyright  2008 二维火科技
+ ***********************************************/
+
+public class ViewInit {
+}
