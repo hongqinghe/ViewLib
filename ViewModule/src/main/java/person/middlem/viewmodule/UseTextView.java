bb1a4a19484f8cf8a3dd80bffe3479f8f193a5f6
gongtong <gongtong@2dfire.com>
2017-11-04 15:43:06 +0800

    添加配置

--- ViewModule/src/main/java/person/middlem/viewmodule/UseTextView.java
+++ ViewModule/src/main/java/person/middlem/viewmodule/UseTextView.java
@@ -1,17 +1,11 @@
 package person.middlem.viewmodule;
-
-import android.view.View;
 import android.widget.TextView;
-
 /***********************************************
  * <P> desc:
  * <P> Author: hehongqing
  * <P> Date: 2017/10/19 22:51
  * <P> Copyright  2008 二维火科技
  ***********************************************/
-
 public class UseTextView {
     TextView textView =new TextView(ViewInit.mContext);
-
-
 }
